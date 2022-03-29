db18015af95a290c64e16470fe25f183fb8aed7d
Ambikesh Singh <ambikeshsingh95@gmail.com>
2022-03-29 11:27:17 +0530

    Add files via upload

---
+++ fibonacci_number.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+
+    if 0<=n<=1:
+        return n
+
+    num,num1=1,0
+    result=None
+    for i in range(n-1):
+        result=num1+num
+        num1=num
+        num=result
+    
+    return result
+
+
+n=int(input("Enter any number : "))
+for j in range(n):
+    print(j,fibonacci(j))
+
+
